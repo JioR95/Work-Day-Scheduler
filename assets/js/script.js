841d025ce2eb2c8f2efdc6d7a0bd7cad93bc743f
JioR95 <jiovani.rivera@rocketmail.com>
2022-07-14 18:38:22 -0400

    script update

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,11 @@ $(document).ready(function(){
 $("#currentDay").html(moment().format("dddd, MMMM Do"));
 });
 
+$(document).ready(function(){
+    $(".saveBtn").on("click", function(){
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+        localStorage.setItem(text, time);
+    })
+    
+}
